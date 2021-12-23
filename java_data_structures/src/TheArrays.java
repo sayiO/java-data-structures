0b3e7727b0119bf58daa78be7b45c1de3c581ef3
onias <sayionias@gmail.com>
2021-12-23 02:04:39 +0200

    working with arrays

---
+++ java_data_structures/src/TheArrays.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class TheArrays {
+    public static void main(String[] args) {
+        //array creation
+        String[] colors = new String[10];
+        colors[0] = "grey";
+        colors[1] = "blue";
+        System.out.println(Arrays.toString(colors));
+
+    }
+}

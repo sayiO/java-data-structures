0f7914d82687649db2ba0936897f87d779b506b9
onias <sayionias@gmail.com>
2021-12-23 02:12:16 +0200

    initialising arrays

--- java_data_structures/src/TheArrays.java
+++ java_data_structures/src/TheArrays.java
@@ -3,9 +3,14 @@ import java.util.Arrays;
 public class TheArrays {
     public static void main(String[] args) {
         //array creation
-        String[] colors = new String[10];
-        colors[0] = "grey";
-        colors[1] = "blue";
+        int []numbers = new int[5];
+        numbers[0] = 34;
+        numbers[1] = 12;
+        numbers[2] = 35;
+        numbers[3] = 40;
+        System.out.println(Arrays.toString(numbers));
+        String[] colors = {"purple", "beige", "cyan", "yellow", "grey"};
+        //printing array contents
         System.out.println(Arrays.toString(colors));
 
     }

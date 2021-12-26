cb68b2775bb75eb7ec1f51942ed8ab50b1e64f68
onias <sayionias@gmail.com>
2021-12-27 00:04:19 +0200

    Working with Stack data structure

---
+++ java_data_structures/src/WorkingWithStacks.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class WorkingWithStacks {
+    public static void main(String[] args){
+        Stack<Integer> st1 = new Stack<>();
+        st1.push(1);
+        st1.push(23);
+        st1.push(34);
+        st1.push(45);
+        st1.push(6);
+        st1.push(89);
+        System.out.println(st1.peek());
+
+    }
+}

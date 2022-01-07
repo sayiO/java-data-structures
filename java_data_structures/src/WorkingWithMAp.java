da7d78155bfb5ee9d1058796275ab1d88899bd4d
onias <sayionias@gmail.com>
2022-01-07 13:43:51 +0200

    iterating techniques for a  map

--- java_data_structures/src/WorkingWithMAp.java
+++ java_data_structures/src/WorkingWithMAp.java
@@ -25,6 +25,29 @@ public class WorkingWithMAp {
         System.out.println(stringMap.containsKey("234"));
         System.out.println(stringMap.containsValue("replacer"));
 
+        stringMap.remove(2);
+        System.out.println(stringMap);
+
+        //iterating the map values
+
+        //using the standard iterator
+        System.out.println("USING STANDARD ITERATOR");
+        Iterator<Integer> keyIterator = stringMap.keySet().iterator();
+        while(keyIterator.hasNext()){
+            Integer nextKey = keyIterator.next();
+            System.out.print(nextKey + ":");
+            System.out.println(stringMap.get(nextKey));
+
+        }
+        //using a for each loop
+        System.out.println("USING FOREACH LOOP");
+
+        for(Integer key: stringMap.keySet()){
+            System.out.print(key + ":");
+            System.out.println(stringMap.get(key));
+        }
+
+
 
 
 

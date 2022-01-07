5ff52cfd16a2fa408326b35128254273f0c5264b
onias <sayionias@gmail.com>
2022-01-07 17:13:39 +0200

    manipulating the deque collection

--- java_data_structures/src/WorkingWithDeque.java
+++ java_data_structures/src/WorkingWithDeque.java
@@ -6,7 +6,7 @@ public class WorkingWithDeque {
         Deque<String> deque1 = new ArrayDeque<>();
         // a deque is an abbreviation for double ended queue, which basically
         // can act in queue mode and stack mode
-        //this means a deque we can use it a standard queue where insertion is at the tail and removal is at the head
+        //this means a deque we can use it as a standard queue where insertion is at the tail and removal is at the head
         //we can also use as stack where insertion and deletion happen at the same position which is the head
 
         //adding values to a deque
@@ -16,8 +16,16 @@ public class WorkingWithDeque {
         deque1.addFirst("C#");
         deque1.addFirst("JavaScript");
         deque1.addLast("Go");
-        deque1.addFirst("Go");
         System.out.println(deque1);
+        System.out.println(deque1.peekFirst());
+        deque1.removeFirst();
+        System.out.println(deque1);
+
+
+        System.out.println(deque1.peek());
+        System.out.println(deque1.peekFirst());
+        System.out.println(deque1.peekLast());
+
 
 
     }

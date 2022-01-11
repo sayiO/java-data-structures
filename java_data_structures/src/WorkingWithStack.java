81e9e10a90f3106abba1802bf3a7b29b2bcc40ca
onias <sayionias@gmail.com>
2022-01-12 00:24:17 +0200

    operations on a Stack

--- java_data_structures/src/WorkingWithStack.java
+++ java_data_structures/src/WorkingWithStack.java
@@ -6,6 +6,20 @@ public class WorkingWithStack {
         st1.push("sayi");
         st1.push(23);
         st1.push(65);
+        st1.push("onias sayi");
+        st1.push("sauka");
+        st1.push("oniasbee");
+
+        st1.pop();
+        System.out.println(st1);
+        System.out.println(st1.peek());
+        Iterator iterator = st1.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+
+
 
     }
 }

69053804e787a88ba84a9237f06ff3b59656517c
onias <sayionias@gmail.com>
2022-01-11 23:57:38 +0200

    working with Stacks

---
+++ java_data_structures/src/WorkingWithStack.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class WorkingWithStack {
+    public static void main(String[]args){
+        Stack st1 = new Stack();
+        st1.push("onias");
+        st1.push("sayi");
+        st1.push(23);
+        st1.push()
+
+    }
+}
